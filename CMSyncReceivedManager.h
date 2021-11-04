b3f80bbb45145839ce05df45befa53f6ea3d09dd
panshaosen <19802021493@139.com>
2021-11-04 09:59:17 +0800

    Delete unneed files

---
+++ CMSyncReceivedManager.h
@@ -0,0 +1,16 @@
+//
+//  CMSyncReceivedManager.h
+//  CMDataSync
+//
+//  Created by 潘绍森 on 2021/11/2.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CMSyncReceivedManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CMSyncSendManager.h
@@ -0,0 +1,16 @@
+//
+//  SyncSendManager.h
+//  CMDataSync
+//
+//  Created by 潘绍森 on 2021/11/2.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CMSyncSendManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END

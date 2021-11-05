1a771ceb9a828fc02b579af79bcc9cca0b968803
panshaosen <19802021493@139.com>
2021-11-05 16:03:02 +0800

    完成链接管理类实现

---
+++ Tool/CMDataSyncTool.m
@@ -0,0 +1,36 @@
+//
+//  CMDataSyncTool.m
+//  CMDataSync
+//
+//  Created by 潘绍森 on 2021/11/5.
+//
+
+#import "CMDataSyncTool.h"
+#import <ifaddrs.h>
+#import <arpa/inet.h>
+
+@implementation CMDataSyncTool
+
++ (NSString *)getCurrentMachineIpAddress {
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    return address;
+}
+
+@end

9fd7f4b961fdfa22c1d62d75fbf57826459051ab
panshaosen <19802021493@139.com>
2021-11-08 11:20:37 +0800

    移动到class内

---
+++ Classes/CMDataSync.h
@@ -0,0 +1,13 @@
+//
+//  CMDataSync.h
+//  CMDataSync
+//
+//  Created by 潘绍森 on 2021/11/8.
+//
+
+#ifndef CMDataSync_h
+#define CMDataSync_h
+#import "CMSyncDefine.h"
+#import "CMSyncTCPConnectManager.h"
+
+#endif /* CMDataSync_h */

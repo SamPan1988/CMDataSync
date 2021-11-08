3d1364cb819478d78626197493ab14a167204e81
panshaosen <19802021493@139.com>
2021-11-08 16:21:09 +0800

    将二维码生成和扫描集成到库中

---
+++ Classes/ServiceDiscover/QRCode/QRCodeServiceManager.h
@@ -0,0 +1,23 @@
+//
+//  QRCodeServiceManager.h
+//  CMDataSync
+//
+//  Created by 潘绍森 on 2021/11/8.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+typedef void(^CMScanComplete)(NSString *);
+
+/// 服务发现类，提供以二维码方式公告服务的存在
+@interface QRCodeServiceManager : NSObject
+
+- (void) startToScanWithView:(UIView *) view complete:(CMScanComplete) scanComplete;
+
+- (UIImage *)generateQRCodeWithString:(NSString *)string Size:(CGFloat)size;
+
+@end
+
+NS_ASSUME_NONNULL_END

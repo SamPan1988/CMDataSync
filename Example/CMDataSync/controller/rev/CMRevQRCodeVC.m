1d507594031047878cd1cd3947ebe852105b1fe9
panshaosen <19802021493@139.com>
2021-11-10 09:50:05 +0800

    接入quick starter

--- Example/CMDataSync/controller/rev/CMRevQRCodeVC.m
+++ Example/CMDataSync/controller/rev/CMRevQRCodeVC.m
@@ -7,8 +7,8 @@
 //
 
 #import "CMRevQRCodeVC.h"
-#import <ifaddrs.h>
-#import <arpa/inet.h>
+#import "ReceiverResolveProtocolManager.h"
+#import <CMDataSync/CMDataSync.h>
 
 @interface CMRevQRCodeVC ()
 @property (nonatomic, strong) UIImageView *qrCodeImageView;
@@ -25,15 +25,10 @@
 - (void)initUI {
     self.title = @"接收数据";
     self.view.backgroundColor = [UIColor whiteColor];
-    
-    NSString *ipAddress = [self getOurIpAddress];
-    NSString *ip = [@"QPen_" stringByAppendingString:ipAddress];
-    UIImage *image = [self generateQRCodeWithString:ip Size:150];
-    
+    UIImage *image = [CMDataSyncQRCodeTCPquickStarter waitForConnectionWithSize:150 receiverResolveProtocol:[ReceiverResolveProtocolManager shared]];
     _qrCodeImageView = [[UIImageView alloc] initWithImage:image];
     [self.view addSubview:_qrCodeImageView];
     _qrCodeImageView.center = CGPointMake(self.view.center.x, self.view.center.y - 30);
-    
     _statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 260, 50)];
     _statusLabel.center = CGPointMake(self.view.center.x, self.view.center.y + 80);
     _statusLabel.numberOfLines = 0;
@@ -43,67 +38,5 @@
     [self.view addSubview:_statusLabel];
 }
 
-//MARK: - Get IP Address
-- (NSString *)getOurIpAddress {
-    NSString *address = @"error";
-    struct ifaddrs *interfaces = NULL;
-    struct ifaddrs *temp_addr = NULL;
-    int success = 0;
-    success = getifaddrs(&interfaces);
-    if (success == 0) {
-        temp_addr = interfaces;
-        while(temp_addr != NULL) {
-            if(temp_addr->ifa_addr->sa_family == AF_INET) {
-                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
-                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
-                }
-            }
-            temp_addr = temp_addr->ifa_next;
-        }
-    }
-    // Free memory
-    freeifaddrs(interfaces);
-    return address;
-}
-
-//生成二维码
-- (UIImage *)generateQRCodeWithString:(NSString *)string Size:(CGFloat)size
-{
-    //创建过滤器
-    CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
-    //过滤器恢复默认
-    [filter setDefaults];
-    //给过滤器添加数据<字符串长度893>
-    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
-    [filter setValue:data forKey:@"inputMessage"];
-    //获取二维码过滤器生成二维码
-    CIImage *image = [filter outputImage];
-    UIImage *img = [self createNonInterpolatedUIImageFromCIImage:image WithSize:size];
-    return img;
-}
-
-//二维码清晰
-- (UIImage *)createNonInterpolatedUIImageFromCIImage:(CIImage *)image WithSize:(CGFloat)size
-{
-    CGRect extent = CGRectIntegral(image.extent);
-    CGFloat scale = MIN(size/CGRectGetWidth(extent), size/CGRectGetHeight(extent));
-    
-    //创建bitmap
-    size_t width = CGRectGetWidth(extent)*scale;
-    size_t height = CGRectGetHeight(extent)*scale;
-    CGColorSpaceRef cs = CGColorSpaceCreateDeviceGray();
-    CGContextRef bitmapRef = CGBitmapContextCreate(nil, width, height, 8, 0, cs, (CGBitmapInfo)kCGImageAlphaNone);
-    CIContext *context = [CIContext contextWithOptions:nil];
-    CGImageRef bitmapImage = [context createCGImage:image fromRect:extent];
-    CGContextSetInterpolationQuality(bitmapRef, kCGInterpolationNone);
-    CGContextScaleCTM(bitmapRef, scale, scale);
-    CGContextDrawImage(bitmapRef, extent, bitmapImage);
-    
-    //保存图片
-    CGImageRef scaledImage = CGBitmapContextCreateImage(bitmapRef);
-    CGContextRelease(bitmapRef);
-    CGImageRelease(bitmapImage);
-    return [UIImage imageWithCGImage:scaledImage];
-}
 
 @end

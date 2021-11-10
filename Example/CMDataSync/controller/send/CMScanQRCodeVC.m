687fc6b57ae5872604ff6c8f81709c49123cdb2e
panshaosen <19802021493@139.com>
2021-11-10 09:50:05 +0800

    接入quick starter

--- Example/CMDataSync/controller/send/CMScanQRCodeVC.m
+++ Example/CMDataSync/controller/send/CMScanQRCodeVC.m
@@ -8,17 +8,11 @@
 
 #import "CMScanQRCodeVC.h"
 #import <AVFoundation/AVFoundation.h>
-
+#import <CMDataSync/CMDataSync.h>
+#import "SenderResolveProtocolManager.h"
 
 
 @interface CMScanQRCodeVC ()<AVCaptureMetadataOutputObjectsDelegate>
-@property ( strong , nonatomic ) AVCaptureDevice * device;
-@property ( strong , nonatomic ) AVCaptureDeviceInput * input;
-@property ( strong , nonatomic ) AVCaptureMetadataOutput * output;
-@property ( strong , nonatomic ) AVCaptureSession * session;
-@property ( strong , nonatomic ) AVCaptureVideoPreviewLayer * preview;
-@property (nonatomic, strong) AVCaptureVideoPreviewLayer *videoPreviewLayer;
-@property (nonatomic, strong) AVCaptureSession *captureSession;
 
 @property (strong, nonatomic) UIView *boxView;
 @property (nonatomic) BOOL isReading;
@@ -41,66 +35,20 @@
     [labIntroudction sizeToFit];
     labIntroudction.center = CGPointMake(self.view.center.x, GH_HEIGHT*0.4+GH_WIDTH*0.5f + 30);
     [self.view addSubview:labIntroudction];
-    
-    _device = [ AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-    _input = [ AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
-    _output = [[ AVCaptureMetadataOutput alloc]init];
-    [ _output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-    
-    _session = [[ AVCaptureSession alloc]init];
-    [ _session setSessionPreset:AVCaptureSessionPresetHigh];
-    if ([ _session canAddInput:self.input]) {
-        [ _session addInput:self.input];
-    }
-    if ([ _session canAddOutput:self.output]) {
-        [ _session addOutput:self.output];
-    }
-    
-    _output.metadataObjectTypes = @[AVMetadataObjectTypeQRCode];
-    [ _output setRectOfInterest : self.view.bounds];
-//    CGRectMake (( 50 )/ GH_HEIGHT ,(( GH_WIDTH - 220 )/ 2 )/ GH_WIDTH , 220 / GH_HEIGHT , 220 / GH_WIDTH )
-    _preview =[AVCaptureVideoPreviewLayer layerWithSession:_session];
-    _preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
-    _preview.frame = self.view.layer.bounds;
-    //    _preview.tor
-    [self.view.layer insertSublayer:_preview atIndex:0];
-    
-    [ _session startRunning ];
-    
-    
 }
 -(void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
+    [CMDataSyncQRCodeTCPquickStarter startConnectionWithScanView:self.view senderResolveProtocol:[SenderResolveProtocolManager shared]];
+}
 
-    [self backAction];
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    [CMDataSyncQRCodeTCPquickStarter stopScanning];
 }
 
 #pragma mark - 取消按钮
--(void)backAction
-{
-    [ _session startRunning];
-}
-#pragma mark - AVCaptureMetadataOutputObjectsDelegate
+-(void)backAction {
 
-- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:( AVCaptureConnection *)connection
-{
-    NSString *stringValue;
-    if ([metadataObjects count] > 0 ) {
-        // 停止扫描
-        [ _session stopRunning ];
-        AVMetadataMachineReadableCodeObject * metadataObject = [metadataObjects objectAtIndex:0];
-        stringValue = metadataObject.stringValue;
-
-        if ([stringValue hasPrefix:@"QPen_"]) {
-            NSLog(@"扫描结果：%@",stringValue);
-            
-//            SendViewController *vc = [SendViewController new];
-//            vc.ip = [stringValue substringFromIndex:5];
-//            [self.navigationController pushViewController:vc animated:YES];
-        } else {
-            NSLog(@"扫描到不符合规则的二维码：%@",stringValue);
-            [self backAction];
-        }
-    }
 }
+
 @end
